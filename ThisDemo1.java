51fc6bd59a6b24ca2b79f2c2d8cb6f28e1c00bf7
FAREED05F4 <130353809+fareed7479@users.noreply.github.com>
2023-09-23 11:15:56 +0530

    Create ThisDemo1.java

---
+++ ThisDemo1.java
@@ -0,0 +1,35 @@
+//java program to demonstrate the importance oof THIS keyword
+//09-09-2023
+//ThisDemo1.java
+import java.io.*;
+class Student
+{
+    int rollno;
+    String name;
+    float fee;
+    Student(int rollno,String name,float fee)
+    {
+        rollno = rollno;
+        name = name;
+        fee= fee;
+    }
+    void display(){
+        System.out.println("Student details are: ");
+        System.out.println(rollno+" "+name+" "+fee);
+    }
+}
+public class ThisDemo1{
+    public static void main(String args[])
+    {
+        Student s1 = new Student(506,"Huseen",2500);
+        Student s2 = new Student (500,"YASH",2400);
+        System.out.println("Students details are : ");
+        s1.display();
+        s2.display();
+    }
+}
+//OUTPUT:-
+//Student details are: 
+//0 null 0.0
+//Student details are: 
+//0 null 0.0
