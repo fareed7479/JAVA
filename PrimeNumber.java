81079cc1ebe3bb6eb7378a00b16caa2a5b1e6349
FAREED05F4 <130353809+fareed7479@users.noreply.github.com>
2023-09-23 11:31:27 +0530

    Add files via upload

---
+++ PrimeNumber.java
@@ -0,0 +1,23 @@
+//java program check whether the given number is prime number
+//23-09-2023
+//PrimeNumber1.java
+public class Main {
+
+  public static void main(String[] args) {
+
+    int num = -17;
+    boolean flag = false;
+    for (int i = 2; i <= num / 2; ++i) {
+      // condition for nonprime number
+      if (num % i == 0) {
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag)
+      System.out.println(num + " is a prime number.");
+    else
+      System.out.println(num + " is not a prime number.");
+  }
+}
